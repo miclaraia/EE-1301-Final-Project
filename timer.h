d8649f486f8a29e2023d1a9c50aacf1d6e3594c5
Michael Laraia <micheal.laraia@gmail.com>
2016-12-10 17:18:23 -0600

    timer

--- timer.h
+++ timer.h
@@ -2,8 +2,11 @@ class MyTimer {
 public:
     unsigned int timer;
     int duration;
-    bool active
-    void *run
+    bool active;
+    void *run();
 
-    MyTimer(unsigned int timer, int duration, void (*run)());
-}
+    bool check();
+    void reset();
+
+    MyTimer(unsigned int timer, int duration);
+};
