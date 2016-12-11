f80c20090efbae6a69f59911083d589ca2543b7a
Michael Laraia <micheal.laraia@gmail.com>
2016-12-11 11:20:22 -0600

    debugging functions

--- debug.cpp
+++ debug.cpp
@@ -1,7 +1,13 @@
 #include "debug.h"
+#include "objects.h"
+
+using std::string;
+using std::stringstream;
+
+void addPin(int pin, stringstream *ss);
 
 void debugButton(int pin) {
-    std::string s;
+    string s;
     switch(pin) {
         case D3:
         s = "D3";
@@ -20,16 +26,52 @@ void debugButton(int pin) {
         break;
     }
 
-    std::stringstream ss;
+    stringstream ss;
     ss << s << " button pressed";
     s = ss.str();
     Serial.print(s.c_str());
     Serial.print("\n");
 }
 
-void debug(std::stringstream *ss) {
+void debug(stringstream *ss) {
     (*ss) << "\n\r";
-    std::string s;
+    string s;
     s = ss->str();
     Serial.print(s.c_str());
+}
+
+void debugArray(List<int> *list) {
+    stringstream ss;
+    ss << "List of presses: ";
+    for (int i = 0; i < list->count; i++) {
+        addPin(list->get(i), &ss);
+        ss << ", ";
+    }
+
+    debug(&ss);
+}
+
+void addPin(int pin, stringstream *ss) {
+    string s;
+    switch(pin) {
+        case D3:
+        s = "D3";
+        break;
+        case D4:
+        s = "D4";
+        break;
+        case D5:
+        s = "D5";
+        break;
+        case D6:
+        s = "D6";
+        break;
+        case D7:
+        s = "D7";
+        break;
+        default:
+        s = "unknown";
+    }
+
+    (*ss) << s;
 }
--- debug.h
+++ debug.h
@@ -2,6 +2,8 @@
 #include <sstream>
 #include <string>
 #include "definitions.h"
+#include "objects.h"
 
 void debugButton(int pin);
 void debug(std::stringstream *ss);
+void debugArray(List<int>);
